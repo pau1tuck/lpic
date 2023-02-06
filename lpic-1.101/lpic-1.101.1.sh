fa19c95f24f14ae2d28cf5132ee75f90df3a4ecf
Paul Tuck <paul.tuck@oxon.org>
2023-02-06 18:42:19 +0700

    "2023-02-06_18:42_ICT+7"

--- lpic-1.101/lpic-1.101.1.sh
+++ lpic-1.101/lpic-1.101.1.sh
@@ -1,6 +1,16 @@
+#!/bin.bash
+
 # In using shell commands, you should also understand shell environment variables, which are placeholders for data that may be useful to many programs.
 
 #  Remember that the shell is a program providing you with an interface to the Linux system.
 
 uname - a
+cd /home/paul
+cd ~
+pwd
+echo Hello, World!
+time pwd    # tells you how long the system took to execute the pwd command.
+# Three times are displayed: total execution time (aka real time), user CPU time, and system CPU time.
+
+set
 
