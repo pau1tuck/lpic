0cf2f5897d334ab3923aa96431bee139305bdfb3
Paul Tuck <paul.tuck@oxon.org>
2023-02-06 18:22:41 +0700

    first commit

---
+++ lpic-1.101/lpic-1.101.1.sh
@@ -0,0 +1,6 @@
+# In using shell commands, you should also understand shell environment variables, which are placeholders for data that may be useful to many programs.
+
+#  Remember that the shell is a program providing you with an interface to the Linux system.
+
+uname - a
+
